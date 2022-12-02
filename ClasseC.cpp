4313fb692ebe0b534a33366ebd3061d77871ee12
Thallys Martins <thallys.henrique.martins@hotmail.com>
2022-12-01 21:29:56 -0300

    Criação da Classe C

---
+++ ClasseC.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+// Classe C
+class C
+{
+private:
+    int C1;
+    float C2;
+
+public:
+    void setC1(int);
+    void setC2(float);
+    int getC1();
+    float getC2();
+    void MC1();
+    void MC2();
+};
+
+int C::getC1()
+{
+    return C1;
+}
+float C::getC2()
+{
+    return C2;
+}
+void C::setC1(int x)
+{
+    C1 = x;
+}
+void C::setC2(float y)
+{
+    C2 = y;
+}
+
+void C::MC1()
+{
+    cout << "MC1";
+}
+
+void C::MC2()
+{
+    cout << "MC2";
+}
+
+
+int main()
+{
+
+    return 0;
+}
